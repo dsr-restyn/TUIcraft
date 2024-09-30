e843f910e1e6c7bb05a3d57e72299418a28a6cc8
Dakota <dakota.secula-rosell@restyn.com>
2024-09-29 22:58:55 -0400

    Fix issue with choice logic

--- pkg/update.go
+++ pkg/update.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmds []tea.Cmd = make([]tea.Cmd, len(m.inputs))
+	// Quit
 	if msg, ok := msg.(tea.KeyMsg); ok {
 		if m.Choice.Name != "New Game" {
 			k := msg.String()
@@ -17,9 +17,21 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 		}
 	}
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		if m.Chosen && m.Choice.Name == "New Game" {
+
+	if !m.Chosen {
+		return m.updateChoices(msg)
+	} else if m.Choice.Name == "New Game" {
+		return m.updateInputs(msg)
+	}
+
+	return m, nil
+}
+
+func (m Model) updateInputs(msg tea.Msg) (Model, tea.Cmd) {
+	var cmds []tea.Cmd = make([]tea.Cmd, len(m.inputs))
+	if m.Chosen && m.Choice.Name == "New Game" {
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
 			switch msg.Type {
 			case tea.KeyEnter:
 				if m.focused == len(m.inputs)-1 {
@@ -27,9 +39,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 					m.Player.Role = m.inputs[1].Value()
 					if err := m.savePlayer(); err != nil {
 						fmt.Println("Error saving player:", err)
-						return m, tick()
 					}
-					return m, tick()
 				}
 			case tea.KeyShiftTab, tea.KeyCtrlP:
 				m.inputs[m.focused].Blur()
@@ -40,56 +50,34 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			case tea.KeyCtrlC:
 				return m, tea.Quit
 			}
-			m.inputs[m.focused].Focus()
-			for i := range m.inputs {
-				m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
-			}
-			return m, tea.Batch(cmds...)
-		} else if m.Chosen && m.Choice.Name == "Load Game" {
-			m.loadPlayer()
-			return m, tick()
 		}
-		k := msg.String()
-		switch k {
-		case "j", "down":
-			if !m.Chosen {
-				id := m.Choice.Id
-				newChoice := m.MenuChoices.GetChoiceById(id + 1)
-				if newChoice.Id != 0 {
-					m.Choice = newChoice
-					return m, tick()
-				} else {
-					return m, tick()
-				}
-			}
-		case "k", "up":
-			if !m.Chosen {
-				id := m.Choice.Id
-				newChoice := m.MenuChoices.GetChoiceById(id - 1)
-				if newChoice.Id != 0 {
-					m.Choice = newChoice
-					return m, tick()
-				} else {
-					return m, tick()
-				}
-			}
-		case "enter":
-			if !m.Chosen {
-				m.Chosen = true
-				return m, tick()
-			}
-
+		m.inputs[m.focused].Focus()
+		for i := range m.inputs {
+			m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
 		}
 
-		return m, tick()
-
-	case frameMsg:
-		return m, frame()
-	case tickMsg:
-		return m, tick()
+	} else if m.Chosen && m.Choice.Name == "Load Game" {
+		m.loadPlayer()
 	}
+	return m, tick()
+}
 
-	return m, nil
+func (m Model) updateChoices(msg tea.Msg) (Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "j", "down":
+			m.nextMenuChoice()
+		case "k", "up":
+			m.previousMenuChoice()
+		case "q", "esc", "ctrl+c":
+			m.Quitting = true
+			return m, tea.Quit
+		case "enter":
+			m.Chosen = true
+		}
+	}
+	return m, tick()
 }
 
 // nextInput focuses the next input field
@@ -108,4 +96,22 @@ func (m *Model) prevInput() {
 	if m.focused < 0 {
 		m.focused = len(m.inputs) - 1
 	}
+}
+
+func (m *Model) nextMenuChoice() {
+	c := m.Choice.Id
+	newChoice := m.MenuChoices.GetChoiceById(c + 1)
+	if newChoice.Id == 0 {
+		newChoice = m.MenuChoices.Choices[0]
+	}
+	m.Choice = newChoice
+}
+
+func (m *Model) previousMenuChoice() {
+	c := m.Choice.Id
+	newChoice := m.MenuChoices.GetChoiceById(c - 1)
+	if newChoice.Id == 0 {
+		newChoice = m.MenuChoices.Choices[len(m.MenuChoices.Choices)-1]
+	}
+	m.Choice = newChoice
 }
